86df71aa0b7810c89d9d353970c1380fa99acec9
allisonsampaio <allisonsampaiox@gmail.com>
2023-04-02 19:09:51 -0300

    fix store_event

--- data/store_event.py
+++ data/store_event.py
@@ -11,25 +11,24 @@ def store_event():
     None
     """
 
-    # Obtém o tipo de evento a partir do GITHUB_CONTEXT
-    github_context = json.loads(os.environ['GITHUB_CONTEXT'])
-    event_type = github_context['event_type']
+    # Obtém o tipo de evento a partir do nome do evento
+    event_name = os.environ['GITHUB_EVENT_NAME']
 
     # Verifica se o tipo de evento é válido
     valid_types = [
         'issues', 'pull_request', 'pull_request_review', 'pull_request_review_comment',
         'push', 'fork', 'gollum', 'issue_comment', 'discussion', 'discussion_comment'
     ]
-    if event_type not in valid_types:
+    if event_name not in valid_types:
         raise ValueError(f"Tipo de evento inválido. Deve ser uma das seguintes opções: {', '.join(valid_types)}.")
 
     # Abre o arquivo JSON correspondente ao tipo de evento
-    filename = f"data/{event_type}.json"
+    filename = f"data/{event_name}.json"
     with open(filename) as json_file:
         data = json.load(json_file)
 
     # Adiciona o evento à lista de eventos do arquivo
-    data[event_type].append(github_context)
+    data[event_name].append(github.context.payload)
 
     # Salva as alterações no arquivo JSON
     with open(filename, 'w') as f:
