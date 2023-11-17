4779ba322708d8302aa31177aceeea75edb59253
allisonsampaio <allisonsampaiox@gmail.com>
2023-11-17 13:08:05 -0300

    refactoring using xp

--- python/models/firebase.py
+++ python/models/firebase.py
@@ -13,6 +13,9 @@ else:
 app = firebase_admin.initialize_app(cred)
 _db:firestore.firestore.Client = firestore.client()
 
+with open("xp.json", "r") as f:
+    xp_map = json.load(f)
+
 def update_user(user:User):
     ref = _db.document(f"users/{user.id}")
     ref.set(user.__dict__)
@@ -20,9 +23,9 @@ def update_user(user:User):
 def add_issue(user:User, data:dict):
     user.issues.append(Issue(data["issue"]))
 
-    # add xp to respective xp_bars
-    user.communication_xp += 2
-    user.continuous_improvement_xp += 1
+    # add xp to respective xp
+    for k in xp_map["issue"]:
+        user.__dict__[k] += xp_map["issue"][k]
 
     update_user(user)
 
@@ -30,76 +33,72 @@ def add_issue(user:User, data:dict):
 def add_issue_comment(user:User, data:dict):
     user.issue_comments.append(IssueComment(data["comment"]))
 
-    # add xp to respective xp_bars
-    user.communication_xp += 1
-    user.cooperation_xp += 1
+    # add xp to respective xp
+    for k in xp_map["issue_comment"]:
+        user.__dict__[k] += xp_map["issue_comment"][k]
 
     update_user(user)
 
 def add_pull_request(user:User, data:dict):
     user.pull_requests.append(PullRequest(data["pull_request"]))
 
-    # add xp to respective xp_bars
-    user.continuous_improvement_xp += 5
-    user.logic_xp += 5
-    user.creativity_xp += 5
+    # add xp to respective xp
+    for k in xp_map["pull_request"]:
+        user.__dict__[k] += xp_map["pull_request"][k]
 
     update_user(user)
 
 def add_pull_request_review(user:User, data:dict):
     user.pr_reviews.append(PR_Review(data["review"]))
 
-    # add xp to respective xp_bars
-    user.cooperation_xp += 5
-    user.continuous_improvement_xp += 5
-    user.logic_xp += 5
+    # add xp to respective xp
+    for k in xp_map["pull_request_review"]:
+        user.__dict__[k] += xp_map["pull_request_review"][k]
 
     update_user(user)
 
 def add_pull_request_review_comment(user:User, data:dict):
     user.pr_review_comments.append(PR_ReviewComment(data["comment"]))
-
-    # add xp to respective xp_bars
-    user.cooperation_xp += 1
-    user.continuous_improvement_xp += 1
+    
+    # add xp to respective xp
+    for k in xp_map["pull_request_review_comment"]:
+        user.__dict__[k] += xp_map["pull_request_review_comment"][k]
 
     update_user(user)
 
 def add_discussion(user:User, data:dict):
     user.discussions.append(Discussion(data["discussion"]))
 
-    # add xp to respective xp_bars
-    user.communication_xp += 2
-    user.creativity_xp += 2
+    # add xp to respective xp
+    for k in xp_map["discussion"]:
+        user.__dict__[k] += xp_map["discussion"][k]
 
     update_user(user)
 
 def add_discussion_comment(user:User, data:dict):
     user.discussion_comments.append(DiscussionComment(data["comment"]))
 
-    # add xp to respective xp_bars
-    user.cooperation_xp += 1
-    user.communication_xp += 1
+    # add xp to respective xp
+    for k in xp_map["discussion_comment"]:
+        user.__dict__[k] += xp_map["discussion_comment"][k]
 
     update_user(user)
 
 def add_push(user:User, *kwargs):
     user.pushes +=1
 
-    # add xp to respective xp_bars
-    user.continuous_improvement_xp += 1
-    user.logic_xp += 1
-    user.creativity_xp += 1
+    # add xp to respective xp
+    for k in xp_map["push"]:
+        user.__dict__[k] += xp_map["push"][k]
 
     update_user(user)
 
 def add_gollum(user:User, *kwargs):
     user.gollums += 1
 
-    # add xp to respective xp_bars
-    user.cooperation_xp += 5
-    user.communication_xp += 1
-    user.creativity_xp += 2
+    # add xp to respective xp
+    for k in xp_map["gollum"]:
+        user.__dict__[k] += xp_map["gollum"][k]
 
     update_user(user)
 
--- python/update_badges.py
+++ python/update_badges.py
@@ -2,12 +2,18 @@ import json
 from models.firebase import update_user
 from models.models import User, Badge
 
+def has_badge(user, badge):
+    titles = [b["title"] for b in user.badges]
+    return badge["title"] in titles
+
 def update_badges(user:User):
-    with open("./python/badges.json", "r") as f:
+    with open("./badges.json", "r") as f:
         badge_data_list = json.load(f)
     
     for badge_data in badge_data_list:
         condition = eval(badge_data["condition"])
         if condition(user):
-            user.badges.append(Badge(badge_data))
-            update_user(user)
+            if not has_badge(user, badge_data):
+                user.badges.append(Badge(badge_data))
+                update_user(user)
+
