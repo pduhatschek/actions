62d702dae52dde4f74773d8c853850f007238d2f
allisonsampaio <allisonsampaiox@gmail.com>
2023-12-04 23:09:14 -0300

    updating challenge

--- python/update_challenges.py
+++ python/update_challenges.py
@@ -5,7 +5,7 @@ from models.models import User
 def update_challenges(user: User):
     is_winner = (
         user.forks > 0
-        and user.pushes > 0
+        #and user.pushes > 0
         and user.pull_requests > 0
         and user.pr_review_comments > 0
         and user.pull_requests_merged > 0
